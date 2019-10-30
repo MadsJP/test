6d50c20d6a771cade144a685dbb55080fe00c6a7
shubhamgopal <s175401@student.dtu.dk>
2019-10-30 14:40:05 +0100

    tilføjet dbHandler klassen

---
+++ src/main/java/DBHandler.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class DBHandler {
+
+    public String getSomedata() throws SQLException, ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://su1.eduhost.dk:3306/server1?user=gruppe6&password=gruppe6");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM patient;");
+        System.out.println("Det virkede");
+        return "Den hemmelige data";
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        new DBHandler().getSomedata();
+
+
+    }
+}
