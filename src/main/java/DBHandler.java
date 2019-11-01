af7d06862473549be892c8373a46cd776657ca0c
Christoffer <s185178@student.dtu.dk>
2019-11-01 10:52:01 +0100

    Opdatering på DBhandler med en patient tilfoejelse

--- src/main/java/DBHandler.java
+++ src/main/java/DBHandler.java
@@ -1,18 +1,36 @@
+import data.Patient;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHandler {
 
-    public String getSomedata() throws SQLException, ClassNotFoundException {
+    public List<Patient> getPatients() throws SQLException, ClassNotFoundException {
         Class.forName("org.mariadb.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mariadb://su1.eduhost.dk:3306/server1?user=gruppe6&password=gruppe6");
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM Patient;");
-        System.out.println("Det virkede");
-        return "Den hemmelige data";
+
+        List<Patient> patients = parseResultsetToPatient(resultSet);
+        System.out.println("Skal oprette en patient og andet");
+        return patients;
+    }
+
+    private List<Patient> parseResultsetToPatient(ResultSet resultSet) throws SQLException {
+        List<Patient> patients = new ArrayList<>();
+        while (resultSet.next()){
+            Patient patient = new Patient();
+            String cpr = resultSet.getString("CPR");
+            patient.setCPR(cpr);
+            patients.add(patient);
+
+        }
+        return patients;
     }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        new DBHandler().getSomedata();
+        new DBHandler().getPatients();
 
     }
 }
---
+++ src/main/java/data/Patient.java
@@ -0,0 +1,14 @@
+package data;
+
+public class Patient {
+    private String CPR;
+
+
+    public void setCPR(String cpr) {
+        this.CPR = cpr;
+    }
+
+    public String getCPR() {
+        return CPR;
+    }
+}
